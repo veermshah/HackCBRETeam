381a385bc8679dd1195bb59d92984eccbf7933ce
jbthelegend <jbthelegend15@gmail.com>
2023-10-11 11:44:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+# Sample list of roles
+roles = ["Technician", "Building Manager", "Account Director", "Chief Engineer", "Portfolio Manager", "Asset Manager", "Leasing Manager", "Facility Coordinator", "Maintenance Supervisor"]
+
+# Create a Streamlet app
+st.title("Role Selection")
+
+# Create a dropdown box for selecting a role
+selected_role = st.selectbox("Select Your Role:", roles)
+
+# Display the selected role
+st.write(f"You selected: {selected_role}")
